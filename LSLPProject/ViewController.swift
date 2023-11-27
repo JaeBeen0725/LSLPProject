a45cf32a34abd24c50faa179892b63afd130680f
Jay <105216574+JaeBeen0725@users.noreply.github.com>
2023-11-27 13:26:19 +0900

    Initial Commit

---
+++ LSLPProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LSLPProject
+//
+//  Created by Jae Oh on 11/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
